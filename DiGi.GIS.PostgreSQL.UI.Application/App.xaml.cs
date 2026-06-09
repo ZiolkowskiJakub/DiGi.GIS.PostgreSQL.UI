99cce67227412364f623fd7da7a24adcac8bc668
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-06-09 11:49:14 +0200

    Error handling added

--- DiGi.GIS.PostgreSQL.UI.Application/App.xaml.cs
+++ DiGi.GIS.PostgreSQL.UI.Application/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace DiGi.GIS.PostgreSQL.UI
 {
@@ -11,12 +12,40 @@ namespace DiGi.GIS.PostgreSQL.UI
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            // Catch exceptions from the main UI thread
+            Current.DispatcherUnhandledException += App_DispatcherUnhandledException;
+
+            // Catch exceptions from other threads
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            // Disable hardware acceleration to diagnose rendering issues
+            System.Windows.Media.RenderOptions.ProcessRenderMode = System.Windows.Interop.RenderMode.SoftwareOnly;
+
             base.OnStartup(e);
 
             // Prevent application from shutting down when all windows are closed
             ShutdownMode = ShutdownMode.OnExplicitShutdown;
 
             gISPostgreSQLTrayApplicationContext = new Classes.GISPostgreSQLTrayApplicationContext();
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Exception currentException = e.Exception;
+            string errorMessage = string.Format("UI Thread Exception: {0}\n{1}", currentException.Message, currentException.StackTrace);
+
+            MessageBox.Show(errorMessage, "Critical UI Error");
+
+            // Prevent the application from crashing immediately to allow for state inspection
+            e.Handled = true;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception currentException = (Exception)e.ExceptionObject;
+            string errorMessage = string.Format("Non-UI Thread Exception: {0}\n{1}", currentException.Message, currentException.StackTrace);
+
+            MessageBox.Show(errorMessage, "Fatal Domain Error");
         }
     }
 }

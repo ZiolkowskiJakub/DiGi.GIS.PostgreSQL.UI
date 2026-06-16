5e560f4ceb617e4db5653f89abe896f8d48d12d8
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-06-16 08:20:53 +0200

    UIBuildingDataUpdateTask  implemented

---
+++ DiGi.GIS.PostgreSQL.UI/Classes/BackgroundTask/UIBuildingDataUpdateTask.cs
@@ -0,0 +1,158 @@
+﻿using DiGi.Core.Classes;
+using DiGi.GIS.PostgreSQL.Interfaces;
+using DiGi.GIS.PostgreSQL.UI.Classes;
+using DiGi.GIS.PostgreSQL.UI.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiGi.GIS.PostgreSQL.Classes
+{
+    /// <summary>
+    /// Represents a background task that updates building data from AdministrativeAreal2D and Building2D sources.
+    /// </summary>
+    public class UIBuildingDataUpdateTask : ReportableBackgroundTask<long>, IGISPostgreSQLObject
+    {
+        /// <summary>
+        /// Gets the GIS PostgreSQL converter manager used to retrieve converters and execute operations.
+        /// </summary>
+        private readonly GISPostgreSQLConverterManager gISPostgreSQLConverterManager;
+
+        public UIBuildingDataUpdateOptions uIBuildingDataUpdateOptions { get; set; } = new UIBuildingDataUpdateOptions();
+
+        /// <summary>
+        /// Constructor with Dependency Injection.
+        /// </summary>
+        /// <param name="gISPostgreSQLConverterManager">The GIS PostgreSQL converter manager used to retrieve converters and execute operations.</param>
+        public UIBuildingDataUpdateTask(GISPostgreSQLConverterManager gISPostgreSQLConverterManager)
+        {
+            this.gISPostgreSQLConverterManager = gISPostgreSQLConverterManager ?? throw new ArgumentNullException(nameof(gISPostgreSQLConverterManager));
+        }
+
+        /// <summary>
+        /// Executes the background task to update building data from AdministrativeAreal2D and Building2D sources.
+        /// </summary>
+        /// <param name="progress">A progress reporter for reporting the number of processed items.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used to cancel the operation.</param>
+        /// <returns>A task representing the asynchronous operation. Returns true if the update was successful; otherwise, false.</returns>
+        protected override async Task<bool> ExecuteAsync(IProgress<long> progress, CancellationToken cancellationToken)
+        {
+            BuildingDataPostgreSQLConverter? buildingDataPostgreSQLConverter = gISPostgreSQLConverterManager.GetPostgreSQLConverter<BuildingDataPostgreSQLConverter>();
+            if (buildingDataPostgreSQLConverter is null)
+            {
+                return false;
+            }
+
+            Building2DPostgreSQLConverter? building2DPostgreSQLConverter = gISPostgreSQLConverterManager.GetPostgreSQLConverter<Building2DPostgreSQLConverter>();
+            if (building2DPostgreSQLConverter is null)
+            {
+                return false;
+            }
+
+            AdministrativeAreal2DPostgreSQLConverter? administrativeAreal2DPostgreSQLConverter = gISPostgreSQLConverterManager.GetPostgreSQLConverter<AdministrativeAreal2DPostgreSQLConverter>();
+            if (administrativeAreal2DPostgreSQLConverter is null)
+            {
+                return false;
+            }
+
+            List<AdministrativeAreal2DReference>? administrativeAreal2DReferences = await administrativeAreal2DPostgreSQLConverter.GetAdministrativeAreal2DReferencesByAdministrativeArealTypeAsync(Enums.AdministrativeArealType.Subdivison, cancellationToken: cancellationToken);
+            if (administrativeAreal2DReferences is null || administrativeAreal2DReferences.Count == 0)
+            {
+                return false;
+            }
+
+            uIBuildingDataUpdateOptions ??= new UIBuildingDataUpdateOptions();
+
+            if(uIBuildingDataUpdateOptions.BuildingDataUpdateTypes is not IEnumerable<BuildingDataUpdateType> buildingDataUpdateTypes || !buildingDataUpdateTypes.Any())
+            {
+                return false;
+            }
+
+            long totalUpdated = 0;
+
+            foreach (AdministrativeAreal2DReference administrativeAreal2DReference in administrativeAreal2DReferences)
+            {
+                if (administrativeAreal2DReference.CountyId is not int countyId)
+                {
+                    continue;
+                }
+
+                List<Building2D>? building2Ds = null;
+                AdministrativeAreal2DReference? administrativeAreal2DReference_Subdivision = null;
+                List<AdministrativeAreal2D>? administrativeAreal2Ds = null;
+
+                List<Building2DReference>? building2DReferences = null;
+
+                try
+                {
+                    AdministrativeAreal2DReferencePath? administrativeAreal2DReferencePath = await administrativeAreal2DPostgreSQLConverter.GetAdministrativeAreal2DReferencePathAsync(administrativeAreal2DReference, cancellationToken);
+
+                    administrativeAreal2DReference_Subdivision = administrativeAreal2DReferencePath?[Enums.AdministrativeArealType.Subdivison];
+
+                    administrativeAreal2Ds = await administrativeAreal2DPostgreSQLConverter.GetAdministrativeAreal2DsByIdsAsync(administrativeAreal2DReferencePath?.AdministrativeAreal2DReferences?.ConvertAll(x => x.Id));
+
+                    building2DReferences = await building2DPostgreSQLConverter.GetBuilding2DReferencesByCountyIdAsync(administrativeAreal2DReference.CountyId.Value, administrativeAreal2DReference_Subdivision?.Id, null, cancellationToken);
+                    if (building2DReferences is null || building2DReferences.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    building2Ds = await building2DPostgreSQLConverter.GetBuilding2DsByBuilding2DReferences(building2DReferences);
+                }
+                catch (Exception)
+                {
+                }
+
+                if (building2Ds is null || building2Ds.Count == 0)
+                {
+                    continue;
+                }
+
+                Core.IO.Table.Classes.Table table = new();
+
+                try
+                {
+                    if(buildingDataUpdateTypes.Contains(BuildingDataUpdateType.General))
+                    {
+                        IO.Modify.Update(table, countyId, administrativeAreal2DReference_Subdivision?.Id, building2Ds?.ConvertAll(x => x.ToDiGi()!), administrativeAreal2Ds: administrativeAreal2Ds?.ConvertAll(x => x.ToDiGi()!));
+                    }
+
+                    if (buildingDataUpdateTypes.Contains(BuildingDataUpdateType.Database))
+                    {
+                        WebAPI.Modify.Update_Id(table, building2DReferences);
+                    }
+                }
+                catch (Exception)
+                {
+                }
+
+                if (table is not null)
+                {
+                    int count = table.RowCount;
+
+                    if (count != 0)
+                    {
+                        bool updated = false;
+                        try
+                        {
+                            updated = await buildingDataPostgreSQLConverter.PushAsync(table);
+                        }
+                        catch (Exception)
+                        {
+                        }
+
+                        if (updated)
+                        {
+                            totalUpdated += count;
+                            progress.Report(totalUpdated);
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- DiGi.GIS.PostgreSQL.UI/Create/VisualBackgroundTasks.cs
+++ DiGi.GIS.PostgreSQL.UI/Create/VisualBackgroundTasks.cs
@@ -1,9 +1,11 @@
-﻿using DiGi.GIS.PostgreSQL.Classes;
+﻿using DiGi.Core;
+using DiGi.GIS.PostgreSQL.Classes;
 using DiGi.GIS.PostgreSQL.UI.Classes;
 using DiGi.GIS.PostgreSQL.UI.Enums;
 using DiGi.GIS.PostgreSQL.WebAPI.Classes;
 using DiGi.UI.WPF.Interfaces;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DiGi.GIS.PostgreSQL.UI
 {
@@ -27,8 +29,40 @@ namespace DiGi.GIS.PostgreSQL.UI
                     result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new PostgreSQLAdministrativeAreal2DCreateDatabaseTask(gISPostgreSQLConverterManager), "Create main database", "Creates main database for AdministrativeAreal2D and Biulding2D objects"));
                     result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new PostgreSQLOrtoDatasCreateDatabaseTask(gISPostgreSQLConverterManager), "Create storage database", "Creates storage database for OrtoDatas objects"));
                     result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new OrtoDatasTask(gISPostgreSQLWebAPIManager, gISPostgreSQLConverterManager), "Bypass upload OrtoDatas from database", "Upload OrtoDatas from database. Direct update of OrtoDatas by bypassing the GIS WebAPI HTTP post."));
-                    result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new PostgreSQLUpdateBuildingDataTask(gISPostgreSQLConverterManager), "Update Building data", "Update Building data base on Buidling2D"));
 
+                    UIBuildingDataUpdateTask uIPostgreSQLUpdateBuildingDataTask = new(gISPostgreSQLConverterManager);
+                    uIPostgreSQLUpdateBuildingDataTask.Starting += (sender, args) => 
+                    {
+                        if (sender is not UIBuildingDataUpdateTask uIPostgreSQLUpdateBuildingDataTask_Temp)
+                        {
+                            return;
+                        }
+
+                        Dictionary<string, BuildingDataUpdateType> dictionary = [];
+                        foreach(BuildingDataUpdateType buildingDataUpdateType in System.Enum.GetValues<BuildingDataUpdateType>())
+                        {
+                            dictionary[buildingDataUpdateType.Description() ?? buildingDataUpdateType.ToString()] = buildingDataUpdateType;
+                        }
+
+                        DiGi.UI.WPF.Windows.ListBoxWindow listBoxWindow = new ("Update building data");
+                        listBoxWindow.SetItems(dictionary.Keys);
+
+                        if(listBoxWindow.ShowDialog() is not bool dialogResult || !dialogResult || listBoxWindow.GetItems<string>() is not List<string> texts)
+                        {
+                            uIPostgreSQLUpdateBuildingDataTask_Temp.uIBuildingDataUpdateOptions.BuildingDataUpdateTypes = [];
+                            return;
+                        }
+
+                        List<BuildingDataUpdateType> buildingDataUpdateTypes = [];
+                        foreach(string text in texts) 
+                        {
+                            buildingDataUpdateTypes.Add(dictionary[text]);
+                        }
+
+                        uIPostgreSQLUpdateBuildingDataTask_Temp.uIBuildingDataUpdateOptions.BuildingDataUpdateTypes = buildingDataUpdateTypes;
+                    };
+
+                    result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(uIPostgreSQLUpdateBuildingDataTask, "Update building data", "Update building data base on Buidling2D and other data sources (database, OrtoDatas etc.)"));
 
                     Building2DPostgreSQLConverter? building2DPostgreSQLConverter = gISPostgreSQLConverterManager.GetPostgreSQLConverter<Building2DPostgreSQLConverter>();
                     if (building2DPostgreSQLConverter is not null)
@@ -44,15 +78,17 @@ namespace DiGi.GIS.PostgreSQL.UI
                         result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new PostgreSQLAdministrativeAreal2DCreateTableTask(administrativeAreal2DPostgreSQLConverter), "Create AdministrativeAreal2D table", "Creates or updates (table indexes etc.) AdministrativeAreal2D table in database"));
                     }
 
-                    result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new PostgreSQLOrtoDatasRefreshTask(gISPostgreSQLConverterManager) 
+                    result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new PostgreSQLOrtoDatasRefreshTask(gISPostgreSQLConverterManager)
                     {
                         PostgreSQLOrtoDatasRefreshOptions = new PostgreSQLOrtoDatasRefreshOptions()
                         {
                             OverrideExistsing = false,
                             UpdateSubdivisionIds = true
                         }
-                    }, 
+                    },
                     "Refresh OrtoDatas", "Refreshes OrtoDatas table in database"));
+
+                    result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new PostgreSQLUpdateOccupancyTask(gISPostgreSQLConverterManager), "Update occupancy from database", "Update occupancy for Building2Ds and AdministrativeAreal2Ds based on data in database"));
                 }
             }
 
@@ -61,7 +97,7 @@ namespace DiGi.GIS.PostgreSQL.UI
                 if (gISPostgreSQLWebAPIManager is not null)
                 {
                     result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new UIUpdateFromFilePostTask(gISPostgreSQLWebAPIManager), "Upload Areal2Ds from BDOT10k file", "Upload AdministrativeAreal2Ds and/or Building2Ds from BDOT10k *.zip file"));
-                    
+
                     result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new UIAdministrativeAreal2DFromFilePostTask(gISPostgreSQLWebAPIManager), "Upload AdministrativeAreal2Ds from file", "Uploads AdministrativeAreal2Ds from file to the server"));
                     result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new UIBuilding2DsFromFilePostTask(gISPostgreSQLWebAPIManager), "Upload Building2Ds from file", "Uploads Building2Ds from file to the server"));
                     result.Add(DiGi.UI.WPF.Create.VisualBackgroundTask(new OrtoDatasFromDatabasePostTask(gISPostgreSQLWebAPIManager), "Upload OrtoDatas from database", "Uploads OrtoDatas from Building2D information stored in the database to the server"));
